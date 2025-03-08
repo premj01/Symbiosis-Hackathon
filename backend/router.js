fbd5b4067e095b5b92da1891890e32a67a8ca236
premj01 <premjadhav00002@gmail.com>
2025-03-08 10:26:55 +0530

    hji

--- backend/router.js
+++ backend/router.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello hi prem  World');
+  try {
+    res.send('Hello hi prem  World');
+
+  } catch (error) {
+    res.send('Hello hi prem  World');
+  }
 });
