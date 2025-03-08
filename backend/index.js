ab15bc528a6f362b65a15803f275c47aa8238ad9
Nikhil Wagh <nwagh008@gmail.com>
2025-03-08 10:14:54 +0530

    v4

--- backend/index.js
+++ backend/index.js
@@ -38,5 +38,26 @@ studyplan = [
         priority: "high",
         tags: ["web development", "html"],
         
+    },
+    {
+        title: "Control ",
+        description: "Learn the basics of HTML",
+        completed: false,
+        dueDate: "2025-03-07",
+        priority: "high",
+        tags: ["web development", "html"],
+        
+    },{
+        title: "HTML",
+        description: "Learn the basics of HTML",
+        completed: false,
+        dueDate: "2025-03-07",
+        priority: "high",
+        tags: ["web development", "html"],
+        
     }
+
+
+
+    
 ]
