1365d54c1fa43061ca2af9c5100b994a0697d237
premj01 <premjadhav00002@gmail.com>
2025-03-08 10:34:41 +0530

    v16

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+const { heroui } = require("@heroui/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()]
+}
