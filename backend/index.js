44bb1864f2f636224099520120e7640b36b8bcab
Nikhil Wagh <nwagh008@gmail.com>
2025-03-08 10:10:47 +0530

    v3

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+studyplan = [
+    {
+         title: "control statements",
+        description: "Learn the basics of HTML",
+        subtopics: [
+            {
+                title: "if",
+                description: "Learn the basics of HTML",
+                completed: false,
+                dueDate: "2025-03-07",
+                reward: 10,
+                priority: "high",
+                tags: ["web development", "html"],
+            },
+            {
+                title: "if else",
+                description: "Learn the basics of HTML",
+                completed: false,
+                dueDate: "2025-03-07",
+                priority: "high",
+                tags: ["web development", "html"],
+            }
+        ],
+        
+    },{
+        title: "Control ",
+        description: "Learn the basics of HTML",
+        completed: false,
+        dueDate: "2025-03-07",
+        priority: "high",
+        tags: ["web development", "html"],
+        
+    },{
+        title: "HTML",
+        description: "Learn the basics of HTML",
+        completed: false,
+        dueDate: "2025-03-07",
+        priority: "high",
+        tags: ["web development", "html"],
+        
+    }
+]
